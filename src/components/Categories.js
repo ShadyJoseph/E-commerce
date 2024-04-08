0b0a4cb36e86515392170a78e5e3ed21f1e92ff7
ShadyJoseph <shadyjosephabadeer2003@gmail.com>
2024-04-08 20:56:09 +0200

    ss

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -6,11 +6,11 @@ import Kid from '../assets/images/kid.avif';
 const Categories = () => {
   return (
     <div className="bg-gray-100">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto py-16 sm:py-24 lg:max-w-none lg:py-32">
-          <h2 className="text-2xl font-bold text-gray-900 mb-8 text-center">Collections</h2>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+          <h2 className="text-2xl font-bold text-gray-900 mb-8">Collections</h2>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div className="group relative">
               <div className="relative h-80 overflow-hidden rounded-lg bg-white">
                 <img src={Man} alt="Man Collection" className="h-full w-full object-cover object-center" />
